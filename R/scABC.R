cfef3847522a6350e45587f5b3e52531247abc2a
timydaley <tdaley@stanford.edu>
2017-05-05 14:33:44 -0700

    updates to main module before I leave for the weekend

--- R/scABC.R
+++ R/scABC.R
@@ -8,9 +8,10 @@
 #' @import GenomicRanges
 #' @output list of cluster assigments, landmarks, peak p-values
 #' 
-scABC <- function(bamfiles, peakfile, plot = FALSE, nClusters = 0, pValThresh = 2, 
+scABC <- function(bamfiles, peakfile, PLOT = FALSE, QUIET = FALSE,
+                  nClusters = c(), pValThresh = 2, 
                   nreadsThresh = 2, ncellsThresh = 10, medianBGthresh = 2, 
-                  lambda = 1, nTop = 2000){
+                  lambda = 1, nTop = 2000, nPerm = 20){
   peaks = select_peaks(peakfile,thresh = pValThresh)
   ForeGround = get_counts_matrix(bamfiles, peaks)
   ForeGroundFiltered = filter_peaks(ForeGround$ForeGround, ForeGround$peaks,
@@ -22,12 +23,37 @@ scABC <- function(bamfiles, peakfile, plot = FALSE, nClusters = 0, pValThresh = 
                                                    median_bg_thresh = medianBGthresh)
   ForeGroundMatrix = ForeGroundBackGroundFiltered$ForeGround
   BackGroundMatrix = ForeGroundBackGroundFiltered$Background
-  if(nClusters > 0){
-    InSilicoLandMarks = compute_landmarks(ForeGround = ForeGroundMatrix, 
-                                          BackGround = BackGroundMatrix, 
-                                          nCluster = nCluster, lambda = lambda, nTop = nTop)
+  BackGroundMedian = apply(BackGroundMatrix, 2, median)
+  if(length(nClusters) == 1){
+    LandMarks = compute_landmarks(ForeGround = ForeGroundMatrix, 
+                                  BackGround = BackGroundMatrix, 
+                                  nCluster = nClusters, lambda = lambda, nTop = nTop)
   }
-  else{ #nClusters unset, so comput nClusters by GapStat
-    
+  else if(length(nClusters) > 1){
+    GapStat = getGapStat(ForeGroundMatrix, BackGroundMedian, 
+                         nClusters=nClusters, nPerm=nPerm, quiet=QUIET)
+    if(PLOT){
+      plotGapStat(GapStat, nClusters=nClusters, main = "Gap Stat")
+    }
+    nClusters = GapStat$nClusterOptimal
+    LandMarks = compute_landmarks(ForeGround = ForeGroundMatrix, 
+                                  BackGround = BackGroundMatrix,
+                                  nCluster = GapStat$nClusterOptimal, 
+                                  lambda = lambda, nTop = nTop)
   }
+  else {
+    GapStat = getGapStat(ForeGroundMatrix, BackGroundMedian, 
+                         nClusters = 1:10, nPerm=nPerm, quiet = QUIET)
+    if(PLOT){
+      plotGapStat(GapStat, nClusters=nClusters, main = "Gap Stat")
+    }
+    nClusters = GapStat$nClusterOptimal
+    LandMarks = compute_landmarks(ForeGround = ForeGroundMatrix, 
+                                  BackGround = BackGroundMatrix,
+                                  nCluster = nClusters, 
+                                  lambda = lambda, nTop = nTop)
+  }
+  LandMarkAssignments = assign2landmarks(ForeGround = ForeGroundMatrix, LandMarks)
+  
+  return(list(ForeGround = ForeGroundMatrix, nClusters = nClusters, LandMarks = LandMarks, ))
 }
